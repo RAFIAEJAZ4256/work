d643654fc09f8dcd14d704514b3a86e681adbbb0
rafiaejaz <rabiyaejaz133@gmail.com>
2024-10-04 05:52:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,125 @@
+
+
+// let str = "Ilovejs";
+// str[1] = "S";
+// console.log(str);
+
+// let marks = [33, 44, 55, 66, 88];
+// console.log(marks);
+// console.log(marks.length);//property
+
+// let heroes = ["rahila", "rabiya", "munifa", "ayesha"];
+// console.log(heroes);
+
+// let fullName = prompt("Enter your full name.");
+// let username = "@" + name;
+// console.log(username);
+
+// // let str = "HELLO";
+// // console.log(str.slice(3));   
+
+// let queen = ["rahila", "rubii", "munifa", "ayesha"];
+// for (let i = 0; i < queen.length; i++) {
+//    console.log(queen[i]);
+// }
+// //let queen = ["gg", "hh", "jjj", "kk", "lll"];
+// //for (let i = 0; i < queen.length; i++) {
+// // console.log(queen[i]);
+// //}  using for loop 
+// //for off ko prefre krynn gy 
+
+// for (let que of queen) {
+//    console.log(queen);
+// }
+
+
+// let cities = ["kashmir", "isl", "pun", "quetta"];
+// for (let city of cities) {
+//    console.log(city);
+// }
+
+// let ave = [67, 44, 89, 77, 35];
+// let sum = 0;
+// for (let val of ave) {
+//    sum += val;
+// }
+
+// console.log(sum);
+
+
+// let mon = [54, 76, 87, 32, 98, 45];
+// let sun = 0;
+// for (let value of mon) {
+//    sun += value;
+// }
+// let avg = sun / mon.length;
+// console.log('avg mon of the class = ${mon}');
+
+// //for of loop
+// let kings = ("aaa", "bbb", "cc", "ddd");
+// for (let king of kings) {
+//    console.log(kings);
+// }
+
+
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+
+// for(let val of marks){
+//    sum += val;
+// }
+// let avg = sum / marks.length;
+
+// console.log(sum);
+
+
+
+// let stud = [56.3, 45.5, 33, 56, 67 ,89];
+// let sum = 0;
+
+// for(let val of stud){
+//    sum += val;
+// }
+// console.log(sum);
+
+
+
+// let studentName = ["rafia", "alishba", "fatima", "munifa", "rabiya"];
+// let studentMarks = [1, 2, 3, 4,]
+// console.log(studentMarks[0]);
+// console.log(studentName.length);
+
+
+// let items = [250, 645, 300, 900, 50];
+
+// let idx = 0;
+// for(let val of items) {
+//    console.log('value of');
+//    idx++;
+// }
+
+// let items = [250, 645, 300, 900];
+// let idx = 0;
+// for (let val of items) {
+//    console.log(items);
+// }
+
+
+// let items = [250, 56, 87, 93, 77];
+
+// for(let i = 0; i < items.length; i++) {
+//    let offer = items[i] / 20;
+// items[4] -= offer;
+// }
+// console.log(items);
+
+
+//practice question2
+let items = [300, 400, 6000, 345];
+for(let i = 0; i < items.length; i++) {
+   let offer = items[i] / 50;
+   items[0] -= offer;
+}
+ console.log(items);
+
+
